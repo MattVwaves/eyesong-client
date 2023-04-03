24658ace5165006e8c99a65a3ec01c1eae715ca9
Matt Holt <jamesmattholt@gmail.com>
2023-04-03 13:43:13 +0100

    Changed life icon

--- src/Components/Hearts.js
+++ src/Components/Hearts.js
@@ -3,20 +3,20 @@ export default function Hearts({ hearts }) {
     <>
       {hearts === 3 && (
         <div className="hearts">
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
         </div>
       )}
       {hearts === 2 && (
         <div className="hearts">
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
         </div>
       )}
       {hearts === 1 && (
         <div className="hearts">
-          <img src={require('../heart02.png')} id="heart" alt="heart" />
+          <img src={require('../Assets/life-3.png')} id="heart" alt="heart" />
         </div>
       )}
     </>
