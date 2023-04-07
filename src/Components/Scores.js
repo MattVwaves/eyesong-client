91e784de13f6bef382e64ea3d7c5a605fe85f16a
Matt Holt <jamesmattholt@gmail.com>
2023-04-07 11:56:58 +0100

    User scoresheets viewable from scores table

--- src/Components/Scores.js
+++ src/Components/Scores.js
@@ -1,14 +1,34 @@
 import { useState, useEffect } from 'react';
+import Youtube from 'react-youtube';
 
 const apiUrl = process.env.REACT_APP_SERVER_URL;
+
+const opts = {
+  height: '100%',
+  width: '100%',
+  playerVars: {
+    autoplay: 0,
+    modestbranding: 1,
+  },
+};
 
 export default function Scores() {
   const [scoreSheets, setScoreSheets] = useState(null);
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [userId, setUserId] = useState(Number(localStorage.getItem('user-id')));
+  const [scoresheetsDisplayed, setScoresheetsDisplayed] = useState([]);
 
-  const handleTotalScore = () => {
-    '10';
+  const handleScoreSheet = (score) => {
+    if (!scoresheetsDisplayed.includes(score.id))
+      setScoresheetsDisplayed([...scoresheetsDisplayed, score.id]);
+    if (scoresheetsDisplayed.includes(score.id)) {
+      const updatedScoresheetsDisplayed = scoresheetsDisplayed.filter(
+        (sheet) => {
+          return sheet !== score.id;
+        }
+      );
+      setScoresheetsDisplayed(updatedScoresheetsDisplayed);
+    }
   };
 
   useEffect(() => {
@@ -43,11 +63,43 @@ export default function Scores() {
       {scoreSheets &&
         scoreSheets.map((score) => {
           return (
-            <div className="scores-container">
-              <p id="your-score">{score.totalScore}</p>
-              <p id="your-score">{score.createdAt}</p>
-              <p id="your-score"> view scoresheet</p>
-            </div>
+            <>
+              <div className="scores-container">
+                <p id="your-score">{score.totalScore}</p>
+                <p id="your-score">{score.createdAt}</p>
+                <p
+                  id="your-score"
+                  className="view-scoresheet"
+                  name={score.id}
+                  onClick={() => handleScoreSheet(score)}
+                >
+                  view scoresheet
+                </p>
+              </div>
+              {scoresheetsDisplayed.includes(score.id) && (
+                <>
+                  {score.songs.map((song) => {
+                    return (
+                      <div
+                        className="scores-container"
+                        id="scoresheet-container"
+                      >
+                        <p id="your-score">{song.songNumber}</p>
+                        <div>
+                          <p id="your-score">
+                            <Youtube videoId={song.videoId} />
+                          </p>
+                        </div>
+                        <p id="your-score">{song.artistName}</p>
+                        <p id="your-score">{song.songTitle}</p>
+                        <p id="your-score">{song.decade}</p>
+                        <p id="your-score">{song.score}</p>
+                      </div>
+                    );
+                  })}
+                </>
+              )}
+            </>
           );
         })}
     </>
